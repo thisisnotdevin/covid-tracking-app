9af11ce2f97c7bad3ccd5548498872ae7e913350
Devin Lin <66978846+thisisnotdevin@users.noreply.github.com>
2022-01-15 15:46:52 -0500

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -1,18 +1,22 @@
-import React from "react";
-import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import React from "react"; // to use jsx
 import "./Map.css";
-import { showDataOnMap } from "./util";
+import { MapContainer, TileLayer } from "react-leaflet";
+import ChangeView from "./ChangeView";
+import { showDataOnMap } from "./utils";
 
 function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
-      <LeafletMap center={center} zoom={zoom}>
+      <MapContainer center={center} zoom={zoom} scrollWheelZoom={false}>
+        {/* this is a component to llsten for changes in center and zoom and sets it because mapcontainer's center and zoom states are immutable  */}
+        <ChangeView center={center} zoom={zoom} />
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
+        {/* loop through all countries and draw circles on map that depend on the number of covid cases; bigger circle = more cases */}
         {showDataOnMap(countries, casesType)}
-      </LeafletMap>
+      </MapContainer>
     </div>
   );
 }
