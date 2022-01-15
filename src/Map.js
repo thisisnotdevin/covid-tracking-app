263891cb1e39085a3dfb706a74c55c84ac27649c
Devin Lin <66978846+thisisnotdevin@users.noreply.github.com>
2022-01-15 15:47:08 -0500

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -8,13 +8,13 @@ function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
       <MapContainer center={center} zoom={zoom} scrollWheelZoom={false}>
-        {/* this is a component to llsten for changes in center and zoom and sets it because mapcontainer's center and zoom states are immutable  */}
+      
         <ChangeView center={center} zoom={zoom} />
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        {/* loop through all countries and draw circles on map that depend on the number of covid cases; bigger circle = more cases */}
+        
         {showDataOnMap(countries, casesType)}
       </MapContainer>
     </div>
